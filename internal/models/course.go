4da9fd9a449596c8f4d65c0a266e037703a693e0
NXTHING <nxthing.az@gmail.com>
2022-07-11 02:05:45 +0400

    implemented migration data mechanism

--- internal/models/course.go
+++ internal/models/course.go
@@ -3,15 +3,16 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Course struct {
-	Id       primitive.ObjectID `json:"id"`
-	Name     string             `json:"name" bson:"name"`
-	Filepath string             `json:"filepath" bson:"filepath"`
-}
-
-type CourseRequest struct {
-	Id         primitive.ObjectID `json:"id" bson:"_id"`
+	Id         primitive.ObjectID `json:"id" bson:"_id"  binding:"required"`
 	Name       string             `json:"name" bson:"name" binding:"required"`
 	Hours      int32              `json:"hours" bson:"hours" binding:"required"`
 	Cost       int32              `json:"cost" bson:"cost" binding:"required"`
 	CourseType string             `json:"courseType" bson:"courseType" binding:"required"`
+}
+
+type CourseRequest struct {
+	Name       string `json:"name" bson:"name" binding:"required"`
+	Hours      int32  `json:"hours" bson:"hours" binding:"required"`
+	Cost       int32  `json:"cost" bson:"cost" binding:"required"`
+	CourseType string `json:"courseType" bson:"courseType" binding:"required"`
 }
--- internal/repos/mongo/mongodb.go
+++ internal/repos/mongo/mongodb.go
@@ -3,6 +3,9 @@ package mongo
 import (
 	"context"
 	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mongodb"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -33,6 +36,17 @@ func ConnectDB() (*mongo.Client, error) {
 	}
 
 	fmt.Println("Connected to MongoDB")
+
+	m, err := migrate.New(
+		"file://./migrations",
+		"mongodb://admin:password@localhost:27017/sstudb?authSource=admin&readPreference=primary&ssl=false")
+
+	err = m.Up()
+
+	if err != nil && err != migrate.ErrNoChange {
+		logrus.Panicln(err.Error())
+	}
+
 	return client, nil
 }
 
