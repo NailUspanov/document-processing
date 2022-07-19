0245ec4fe6687192ffa1ab06deed0193e5bb9c5c
Наиль <nailuspnov@gmail.com>
2022-07-19 11:48:19 +0400

    enable cors

--- internal/handlers/rest/handler.go
+++ internal/handlers/rest/handler.go
@@ -16,6 +16,19 @@ func NewHandler(services *services.Service) *Handler {
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
 	auth := router.Group("/auth")
 	{
 		//auth.POST("/sign-up", h.signUp)
